57cd3be5b652290b26484d74e0f2afc1a800be42
叶作垚 <yezuoyao@xiezuoyaodeMacBook-Pro.local>
2019-03-25 18:07:48 +0800

    mqtt

---
+++ src/main/java/com/bupt/devicesaccess/config/MqttConfiguration.java
@@ -0,0 +1,109 @@
+package com.bupt.devicesaccess.config;
+
+import com.bupt.devicesaccess.mqtt.MqttPushClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Description:  ---——require需求|ask问题|jira
+ * Design :  ----the  design about train of thought 设计思路
+ * User: yezuoyao
+ * Date: 2019-03-25
+ * Time: 16:48
+ * Email:yezuoyao@huli.com
+ *
+ * @author yezuoyao
+ * @since 1.0-SNAPSHOT
+ */
+@Component
+@Configuration
+public class MqttConfiguration {
+    @Value("${mqtt.host}")
+    private String host;
+
+    @Value("${mqtt.client-id}")
+    private String clientId;
+
+    @Value("${mqtt.username}")
+    private String userName;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Value("${mqtt.topic}")
+    private String[] topic;
+
+    @Value("${mqtt.timeout}")
+    private int timeout;
+
+    @Value("${mqtt.keepalive}")
+    private int keepalive;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String[] topic) {
+        this.topic = topic;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getKeepalive() {
+        return keepalive;
+    }
+
+    public void setKeepalive(int keepalive) {
+        this.keepalive = keepalive;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+    @Bean
+    public MqttPushClient getMqttPushClient(){
+        MqttPushClient mqttPushClient = new MqttPushClient();
+        mqttPushClient.connect(host, clientId, userName, password, topic, timeout,keepalive);
+        return mqttPushClient;
+    }
+
+}
--- src/main/java/com/bupt/devicesaccess/controller/TestController.java
+++ src/main/java/com/bupt/devicesaccess/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.bupt.devicesaccess.controller;
 
 import com.bupt.devicesaccess.aop.Token;
+import com.bupt.devicesaccess.mqtt.MqttPushClient;
 import com.bupt.devicesaccess.utils.JsonResponseUtil;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,10 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class TestController {
+    @Autowired
+    private MqttPushClient mqttPushClient;
+
     @RequestMapping(value = "/",method = RequestMethod.GET)
     @ApiOperation(value="mainUrl", notes="这只是一个测试controller调用的接口，没有任何的业务逻辑")
     public String mainUrl(){
         return JsonResponseUtil.ok("欢迎使用 devices-access，请访问swagger-ui.html获取接口信息");
     }
+
+    @RequestMapping(value = "/mqtt/publish",method = RequestMethod.GET)
+    @ApiOperation(value="mqttPublish", notes="mqtt发送Publish,默认qos为0")
+    public String mqttPublish(@RequestParam(value = "messange", defaultValue = "hello mqtt server")  String messange){
+        mqttPushClient.publish("hello",messange);
+        return JsonResponseUtil.ok("6666");
+    }
+
+
+
 
 }
